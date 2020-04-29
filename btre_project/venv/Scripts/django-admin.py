08d389393e364942b2cf6c21bf1c35323f4ab1bc
VeXuZ <vexuzdorotan@gmail.com>
2020-04-29 14:31:45 +0800

    'btre_project'

---
+++ btre_project/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\vexuz\local google drive\udemy_python_django_dev_to_deployment\btre_project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
